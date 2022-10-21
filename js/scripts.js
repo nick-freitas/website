e507072fd2a31ffc5d03b267e7f2faf6d53c9c80
Nick Freitas <nickfrei@gmail.com>
2022-10-20 20:22:21 -0400

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,4 @@
-/*!
-* Start Bootstrap - Resume v7.0.1 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
-
     // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
@@ -16,7 +6,7 @@ window.addEventListener('DOMContentLoaded', event => {
             target: '#sideNav',
             offset: 74,
         });
-    };
+    }
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
